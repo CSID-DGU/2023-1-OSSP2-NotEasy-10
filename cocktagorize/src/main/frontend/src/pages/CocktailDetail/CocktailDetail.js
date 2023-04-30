ea212ea669b7c0f631d64b5828b0a73ccd31a195
Jeong3733 <wjdwodnr445@naver.com>
2023-04-30 21:11:12 +0900

    React router dom, styled component 설치, app.js router 설정

---
+++ cocktagorize/src/main/frontend/src/pages/CocktailDetail/CocktailDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CocktailDetail = () => {
+  return <div>칵테일 상세정보 페이지</div>;
+};
+
+export default CocktailDetail;
---
+++ cocktagorize/src/main/frontend/src/pages/MyPage/MyPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyPage = () => {
+  return <div>마이페이지</div>;
+};
+
+export default MyPage;
