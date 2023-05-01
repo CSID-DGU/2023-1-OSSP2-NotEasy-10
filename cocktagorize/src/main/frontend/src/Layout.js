43229148d225e94efff129f2e1cc27295679ba2c
euneun17 <0617ye@gmail.com>
2023-05-01 20:31:42 +0900

    Layout.js 변경

--- cocktagorize/src/main/frontend/src/Layout.js
+++ cocktagorize/src/main/frontend/src/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./component/common/Header";
 import Footer from "./component/common/Footer";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./component/common/sidebar/Sidebar";
 
 const Layout = () => {
   return (
@@ -10,7 +9,6 @@ const Layout = () => {
       <Header />
       <main>
         <Outlet />
-        <Sidebar></Sidebar>
       </main>
       <Footer />
     </div>
