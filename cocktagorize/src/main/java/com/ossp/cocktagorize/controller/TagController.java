15e9e5c6567d10f801ef4c00da7f21da6e881fd6
dudongtak <ytleea98@gmail.com>
2023-05-16 14:56:44 +0900

    tag 전체 보내주기

--- cocktagorize/src/main/java/com/ossp/cocktagorize/controller/TagController.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/controller/TagController.java
@@ -5,6 +5,7 @@ import com.ossp.cocktagorize.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Controller
+@CrossOrigin
 @RestController
 public class TagController {
     @Autowired
