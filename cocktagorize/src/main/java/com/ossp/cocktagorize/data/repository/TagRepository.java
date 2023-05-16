37b850be880cba7424ad456b3047b66d56054dc9
dudongtak <ytleea98@gmail.com>
2023-05-16 13:01:00 +0900

    tag 전체 보내주기

--- cocktagorize/src/main/java/com/ossp/cocktagorize/data/repository/TagRepository.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/data/repository/TagRepository.java
@@ -1,11 +1,12 @@
 package com.ossp.cocktagorize.data.repository;
 
 import com.ossp.cocktagorize.data.entity.Tag;
-import com.ossp.cocktagorize.data.type.TagType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import reactor.util.annotation.NonNullApi;
 
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
     Tag findTagByName(String name);
+    List<Tag> findAll();
 }
