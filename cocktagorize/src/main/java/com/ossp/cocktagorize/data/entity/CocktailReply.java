32ef5dac4e938674846dd7728fc801022f8a3540
dudongtak <ytleea98@gmail.com>
2023-05-20 13:18:01 +0900

    cocktail 좋아요 기능

--- cocktagorize/src/main/java/com/ossp/cocktagorize/data/entity/CocktailReply.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/data/entity/CocktailReply.java
@@ -1,5 +1,6 @@
 package com.ossp.cocktagorize.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
