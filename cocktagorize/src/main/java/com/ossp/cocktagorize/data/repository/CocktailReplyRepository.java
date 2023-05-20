968f30cd3619e77be33dc2dd6f431034225b64cc
dudongtak <ytleea98@gmail.com>
2023-05-20 23:02:45 +0900

    cocktaildetail 댓글 수정 삭제

--- cocktagorize/src/main/java/com/ossp/cocktagorize/data/repository/CocktailReplyRepository.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/data/repository/CocktailReplyRepository.java
@@ -1,6 +1,7 @@
 package com.ossp.cocktagorize.data.repository;
 
 import com.ossp.cocktagorize.data.entity.CocktailReply;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface CocktailReplyRepository extends JpaRepository<CocktailReply,Integer> {
      @SuppressWarnings("all")
      CocktailReply save(CocktailReply cocktailReply);
+     @Transactional
+     void deleteById(int id);
+     CocktailReply findById(int replyId);
 }
