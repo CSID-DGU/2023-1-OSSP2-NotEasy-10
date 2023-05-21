17e0d3b9c774cea1b7b585870cc9182808692924
juhno1023 <hyeondnr1023@gmail.com>
2023-05-21 12:55:08 +0900

    Favorites 페이지 기능 구현 (칵테일 + 게시판)

--- cocktagorize/src/main/java/com/ossp/cocktagorize/controller/FavoritesPageController.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/controller/FavoritesPageController.java
@@ -1,25 +1,24 @@
 package com.ossp.cocktagorize.controller;
-import com.ossp.cocktagorize.data.dto.CocktailResponseDto;
 import com.ossp.cocktagorize.data.dto.FavoritePageResponseDto;
 import com.ossp.cocktagorize.data.dto.UsernameRequestDto;
 import com.ossp.cocktagorize.data.entity.*;
-import com.ossp.cocktagorize.data.idClass.UserLikeCocktailId;
+
 import com.ossp.cocktagorize.data.repository.UserLikeBoardRepository;
 import com.ossp.cocktagorize.data.repository.UserLikeCocktailRepository;
 import com.ossp.cocktagorize.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 public class FavoritesPageController {
+
     private final UserRepository userRepository;
+
     private final UserLikeCocktailRepository userLikeCocktailRepository;
 
     private final UserLikeBoardRepository userLikeBoardRepository;
