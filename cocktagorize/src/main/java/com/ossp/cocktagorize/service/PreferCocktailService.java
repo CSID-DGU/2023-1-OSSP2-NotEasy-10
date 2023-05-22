b138ec1292f7df07c6e79f7e2b2fe20048e9787c
Jeong3733 <wjdwodnr445@naver.com>
2023-05-22 22:34:56 +0900

    날씨 기반 / 사용자 기반 추천 칵테일 UserLike 안 담은거 수정

--- cocktagorize/src/main/java/com/ossp/cocktagorize/service/PreferCocktailService.java
+++ cocktagorize/src/main/java/com/ossp/cocktagorize/service/PreferCocktailService.java
@@ -5,6 +5,8 @@ import com.ossp.cocktagorize.data.entity.*;
 import com.ossp.cocktagorize.data.repository.*;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -22,6 +24,8 @@ public class PreferCocktailService {
     private PreferTagRepository preferTagRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserLikeCocktailRepository userLikeCocktailRepository;
 
     @Transactional
     public List<CocktailResponseDto> getPreferTagCocktail(String username){
@@ -34,9 +38,13 @@ public class PreferCocktailService {
         }
         Random random=new Random();
         random.setSeed(System.currentTimeMillis());
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
         for(int i=0;i<3;i++){
             int n=random.nextInt(tags.size());
-            preferCocktail.add(new CocktailResponseDto(cocktailRepository.findById(tags.get(n).getCocktail().getId())));
+            Cocktail cocktail = cocktailRepository.findById(tags.get(n).getCocktail().getId());
+            preferCocktail.add(new CocktailResponseDto(cocktail, userLikeCocktailRepository.findByCocktailIdAndUserId(cocktail.getId(), userRepository.findByUsername(authentication.getName()).getId())));
             tags.remove(n);
         }
         return preferCocktail;
