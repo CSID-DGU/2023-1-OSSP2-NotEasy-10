71d5bbcb02596868c5fa8073165e6dea900b9f42
Birdcantfly <71496474+ner-di-dream@users.noreply.github.com>
2023-05-22 22:31:44 +0900

    [5/22] Home 수정

--- cocktagorize/src/main/frontend/src/pages/Home/HomeCss.js
+++ cocktagorize/src/main/frontend/src/pages/Home/HomeCss.js
@@ -167,6 +167,21 @@ export const SearchOptionBase = styled.option`
 	font-size: 18px;
 `;
 
+export const WeatherSearchOption = styled.select`
+	width: 150px;
+	height: 30px;
+	border-width: 2px;
+	border-radius: 15px;
+	margin: 0px 5px;
+	padding: 0px 10px;
+	border-color: black;
+	font-size: 12px;
+`;
+
+export const WeatherSearchOptionBase = styled.option`
+	font-size: 12px;
+`;
+
 export const LoginContent = styled.div`
 	display: flex;
 	flex-direction: column;
@@ -202,6 +217,9 @@ export const WeatherInfoBox = styled.div`
 	border: solid;
 	border-radius: 5px;
 	border-color: black;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 `;
 
 export const WeatherInfo = styled.p`
@@ -461,4 +479,34 @@ export const blackXButton = styled.img`
 	float: left;
 	-webkit-user-drag: none;
 	-webkit-user-select: none;
+`;
+
+export const WeatherLoading = styled.div`
+	width: 15.625vw;
+	height: calc(75vh - 270px);
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const WeatherLoadingImage = styled.img`
+	width: 75px;
+	height: 75px;
+	-webkit-user-drag: none;
+	-webkit-user-select: none;
+	animation: rotate_image 1s ease-out infinite;
+	transform-origin: 50% 50%;
+
+	@keyframes rotate_image {
+		0% {
+			transform: rotate(0deg);
+		}
+		50% {
+			transform: rotate(180deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
 `;
