239855b0ad4ac3f36ecac214174c80e03c0300eb
euneun17 <0617ye@gmail.com>
2023-05-22 10:05:19 +0900

    커뮤니티 조회,수정,작성 page 완성(데이터 연동X)

--- cocktagorize/src/main/frontend/src/pages/community/CommunityPost.js
+++ cocktagorize/src/main/frontend/src/pages/community/CommunityPost.js
@@ -3,28 +3,37 @@ import Sidebar from '../../component/common/sidebar/Sidebar';
 import CocktailCard from "../../component/cocktailCard";
 import './CommunityPost.css'
 import UserTipList from "../../component/UserTipList";
+import PostTag from "../../component/common/PostTag";
+import {Link} from "react-router-dom";
+import { VscHeartFilled} from "react-icons/vsc";
 
 const CommunityPost = () => {
+
+  const handleDelete = () => {
+    // axios로 삭제요청 보내면 서버에서 boardList 업데이트
+  };
 
   return (
     <div className="CommunityPost">
       <Sidebar />
       <div className="wrap">
         <div className="post_wrap">
-          {/* <type컴포넌트/> */}<span className="post_title">title </span>
+          <div className="post_top">
+            <PostTag type="board.type"/> <span className="post_title">board.title</span>
+          </div>
           <div className="post_info">
-            <p className="post_name">nickname</p> <p className="post_like">like</p> <p className="post_time">created</p>
+            <p className="post_name">board.user.name</p> <p className="post_like"><VscHeartFilled/>board.liked</p> <p className="post_time">board.created</p>
           </div>
           <hr />
           <div className="post_content">
-            content
+            board.content
           </div>
           <div className="onlyUser">
-            <button className="post_edit">edit</button>
-            <button className="post_delete">delete</button>
+            {/* <Link to={`/community/${board.id}/modify`}>*/}<button className="post_edit">수정</button>{/* </Link>*/}
+            <button className="post_delete" onClick={handleDelete}>삭제</button>
           </div>
         </div>
-        <UserTipList/>
+        {/* <UserTipList tips={boardReplyList}/> */}
       </div>
       <CocktailCard />
     </div>
